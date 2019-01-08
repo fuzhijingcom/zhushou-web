cee7a7187b5c9b6c89c1c7c7fbe37580245dc0a6
管理员 <www@lang100.com>
2019-01-08 23:05:49 +0800

    商品编辑

---
+++ application/shop/controller/Goods.php
@@ -0,0 +1,100 @@
+<?php
+namespace app\shop\controller;
+
+use think\Controller;
+
+//商品
+class Goods extends Controller
+{
+    public function _initialize()
+    {
+        //检查是否登录
+        if ($this->request->action() != 'logout') {
+            //排除 logout
+            $login = new \app\common\controller\Login();
+            $login->check_login();
+        }
+    }
+
+    public function index()
+    {
+        $token = session("token");
+        $seller_id = session("seller_id");
+        $schoolname = session("schoolname");
+
+        $url = CONFIG('api5_url')."school/admin/goods_list?seller_id=".$seller_id;
+        $res = httpRequest($url,'GET');
+        if(!$res){
+            $this->error('请求出错');
+        }
+        $res = json_decode($res,true);
+        
+        if($res['status'] == 1){
+            $data = $res['data'];
+        }else{
+            $this->error($res['msg']);
+        }
+
+        $this->assign('data', $data);
+
+        return $this->fetch();
+    }
+
+
+    /**
+     * 编辑商品
+     */
+    public function edit(){
+        
+        $goods_id = input('goods_id');
+
+        $url = CONFIG('api5_url')."school/admin/goods_detail?goods_id=".$goods_id;
+        $res = httpRequest($url,'GET');
+        if(!$res){
+            $this->error('请求出错');
+        }
+        $res = json_decode($res,true);
+      
+        if($res['status'] == 1){
+            $data = $res['data'];
+        }else{
+            $this->error($res['msg']);
+        }
+
+        $this->assign('data', $data);
+
+        return $this->fetch();
+
+    }
+
+    /**
+     * 编辑 post
+     */
+    public function edit_post(){
+        $goods_id = input('goods_id');
+
+        $goods_name = input('goods_name');
+        $price=input('price');
+        $packing_fee=input('packing_fee');
+        $sales=input('sales');
+        $stock=input('stock');
+        $detail=input('detail');
+
+        $url = CONFIG('api5_url')."school/admin/goods_edit?goods_id=".$goods_id."&goods_name=".$goods_name."&price=".$price."&packing_fee=".$packing_fee."&sales=".$sales."&stock=".$stock."&detail=".$detail;
+        $res = httpRequest($url,'GET');
+      
+        $res = json_decode($res,true);
+       
+        if(!$res){
+            $this->error('请求出错');
+        }
+      
+        if($res['status'] == 1){
+            $this->success($res['msg'],'index');
+        }else{
+            $this->error($res['msg']);
+        }
+    }
+
+
+}
